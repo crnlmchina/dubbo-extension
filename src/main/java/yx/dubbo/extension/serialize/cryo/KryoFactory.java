22b04af5f44fdf4f29ed5856899368dc0eb512bc
wangyuxuan <crnlmcn@gmail.com>
2014-05-20 23:42:40 +0800

    add dependency kryo-serializers

--- src/main/java/yx/dubbo/extension/serialize/cryo/KryoFactory.java
+++ src/main/java/yx/dubbo/extension/serialize/cryo/KryoFactory.java
@@ -1,13 +1,23 @@
 package yx.dubbo.extension.serialize.cryo;
 
+import java.lang.reflect.InvocationHandler;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-
-import yx.dubbo.extension.serialize.cryo.serializer.ArrayListSerializer;
-import yx.dubbo.extension.serialize.cryo.serializer.HashMapSerializer;
+import java.util.Collections;
+import java.util.GregorianCalendar;
 
 import com.esotericsoftware.kryo.Kryo;
+
+import de.javakaffee.kryoserializers.ArraysAsListSerializer;
+import de.javakaffee.kryoserializers.CollectionsEmptyListSerializer;
+import de.javakaffee.kryoserializers.CollectionsEmptyMapSerializer;
+import de.javakaffee.kryoserializers.CollectionsEmptySetSerializer;
+import de.javakaffee.kryoserializers.CollectionsSingletonListSerializer;
+import de.javakaffee.kryoserializers.CollectionsSingletonMapSerializer;
+import de.javakaffee.kryoserializers.CollectionsSingletonSetSerializer;
+import de.javakaffee.kryoserializers.GregorianCalendarSerializer;
+import de.javakaffee.kryoserializers.JdkProxySerializer;
+import de.javakaffee.kryoserializers.SynchronizedCollectionsSerializer;
+import de.javakaffee.kryoserializers.UnmodifiableCollectionsSerializer;
 
 /**
  * @author yuxuan
@@ -23,9 +33,17 @@ public final class KryoFactory {
 	}
 
 	protected void register(Kryo kryo) {
-		kryo.register(Map.class, new HashMapSerializer());
-		kryo.register(Arrays.asList().getClass(), new ArrayListSerializer());
-		kryo.register(List.class, new ArrayListSerializer());
+		kryo.register(Arrays.asList("").getClass(), new ArraysAsListSerializer());
+		kryo.register(Collections.EMPTY_LIST.getClass(), new CollectionsEmptyListSerializer());
+		kryo.register(Collections.EMPTY_MAP.getClass(), new CollectionsEmptyMapSerializer());
+		kryo.register(Collections.EMPTY_SET.getClass(), new CollectionsEmptySetSerializer());
+		kryo.register(Collections.singletonList("").getClass(), new CollectionsSingletonListSerializer());
+		kryo.register(Collections.singleton("").getClass(), new CollectionsSingletonSetSerializer());
+		kryo.register(Collections.singletonMap("", "").getClass(), new CollectionsSingletonMapSerializer());
+		kryo.register(GregorianCalendar.class, new GregorianCalendarSerializer());
+		kryo.register(InvocationHandler.class, new JdkProxySerializer());
+		UnmodifiableCollectionsSerializer.registerSerializers(kryo);
+		SynchronizedCollectionsSerializer.registerSerializers(kryo);
 	}
 
 }
